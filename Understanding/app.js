6c9fbb75de2a3b66148cdecfb8b490714da9598b
Dana <djsilvertail@byui.edu>
2021-09-28 08:05:02 -0700

    even more catch up

--- Understanding/app.js
+++ Understanding/app.js
@@ -1,39 +1,9 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    //console.log(req.url, req.method, req.headers);
-    //process.exit(); (Stops listener/shuts down server)
+const routes = require('./routes');
 
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>My Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type"text" name"message"><button type"submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-        });
-        fs.writeFileSync('message.txt', 'DUMMY');
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end;
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Welcome To My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
+
 server.listen(3000);
