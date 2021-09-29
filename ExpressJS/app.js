c6649a91144a2a419fb1c577f5c352194f8cf0bf
Dana <djsilvertail@byui.edu>
2021-09-29 03:45:12 -0700

    practicing express with an assignment

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -1,5 +1,24 @@
-const http = require('http');
+//const http = require('http'); Not needed when using app.listen()
 
-const server = http.createServer();
+const express =require('express');
 
-server.listen(3000);
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next();
+});
+
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Add Product Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express</h1>');
+});
+
+//const server = http.createServer(app);
+//server.listen(3000);
+app.listen(3000);// This works the same as the previous two lines.
